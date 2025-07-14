a06147543bb755ed845ff211786f6af35c68778f
Anastasia Kvasha <nastyakvsh@gmail.com>
2025-07-14 22:23:20 +0300

    work on mistakes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,10 @@ import md.cvasha.contacts.model.ContactManager;
 import java.util.Scanner;
 
 public class Main {
+  static ContactManager manager = new ContactManager();
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    ContactManager manager = new ContactManager();
     int contactId = 1;
 
     while (true) {
@@ -15,7 +16,7 @@ public class Main {
 """
 1. Add Contact
 2. View All Contacts
-3. Delete Contact by full data
+3. Delete Contact by Id
 4. Exit
 Choose your option:
 """);
@@ -25,42 +26,15 @@ Choose your option:
 
       switch (ch) {
         case 1 -> {
-          System.out.print("First Name: ");
-
-
-          String firstName = sc.nextLine();
-
-          System.out.print("Last Name: ");
-          String lastName = sc.nextLine();
-
-          System.out.print("Phone Number: ");
-          String phoneNumber = sc.nextLine();
-
-          System.out.print("Email: ");
-          String email = sc.nextLine();
-
-          Contact contact = new Contact(contactId++, firstName, lastName, phoneNumber, email);
-          manager.addContact(contact);
-          System.out.println("Contact added!");
+          addContact();
         }
 
         case 2 -> {
-          System.out.println("All Contacts:");
-          manager.printAllContacts();
+          viewAllContacts();
         }
 
         case 3 -> {
-          System.out.println("Remove contact by Id:");
-          System.out.print("Enter Id: ");
-          int idToDel = sc.nextInt();
-
-          boolean removedByData = manager.removeByID(idToDel);
-
-          if (removedByData) {
-            System.out.println("Contact deleted.");
-          } else {
-            System.out.println("The contact is not found.");
-          }
+          deleteContact();
         }
         case 4 -> {
           System.out.println("Goodbye!");
@@ -69,6 +43,48 @@ Choose your option:
 
         default -> System.out.println("Try again!");
       }
+    }
+  }
+
+  public static void addContact() {
+    Scanner sc = new Scanner(System.in);
+    int contactId = 0;
+
+    System.out.print("First Name: ");
+
+    String firstName = sc.nextLine();
+
+    System.out.print("Last Name: ");
+    String lastName = sc.nextLine();
+
+    System.out.print("Phone Number: ");
+    String phoneNumber = sc.nextLine();
+
+    System.out.print("Email: ");
+    String email = sc.nextLine();
+
+    Contact contact = new Contact(contactId++, firstName, lastName, phoneNumber, email);
+    manager.addContact(contact);
+    System.out.println("Contact added!");
+  }
+
+  public static void viewAllContacts() {
+    System.out.println("All Contacts:");
+    manager.printAllContacts();
+  }
+
+  public static void deleteContact() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Remove contact by Id:");
+    System.out.print("Enter Id: ");
+    int idToDel = sc.nextInt();
+
+    boolean removedByData = manager.removeByID(idToDel);
+
+    if (removedByData) {
+      System.out.println("Contact deleted.");
+    } else {
+      System.out.println("The contact is not found.");
     }
   }
 }
